e37a8a86898ab50496ec713ae7a617024b6a7ab8
Alexander Woodward <alex.w.nz@gmail.com>
2023-08-31 21:33:53 +0900

    Update gifti_surface_transformation.py

--- gifti_surface_transformation.py
+++ gifti_surface_transformation.py
@@ -1,51 +1,61 @@
-""" Apply ANTs generated transformations to vertices of a GIfTI file.
+"""Apply ANTs generated transformations to vertices of a GIfTI file.
 
-This script allows the user to transform a surface mesh, given as a GIfTI file, 
-from one space to another. The code has been used on 3D brain surface 
-mesh data. The transforms were generated from registering one 3D brain volume 
-(where the surface was defined) to another using the ANTs normalization tools.
+This script transforms a surface mesh, given as a GIfTI file, from one space to another. It is specifically designed for 3D brain surface mesh data. The transformations are generated using the ANTs normalization tools.
 
 Authors:
     Rui Gong, Alexander Woodward - 2023/09/27
 """
+
 import SimpleITK as sitk
 import nibabel as nib
 
 
 def transform_gifti_between_spaces(in_filename, out_filename, affine_tfm, inv_warp_tfm):
-    """Transform the vertices in a gifti file from one space to another.
+    """Transform the vertices in a GIfTI file from one space to another.
 
-    Parameters:
-    in_filename -- the input filename.
-    out_filename -- the output filename.
-    affine_tfm -- The filename of the affine component of the transform.
-    inv_warp_tfm -- The filename of the non-linear warp component of the transform.
+    Args:
+        in_filename (str): The input filename of the GIfTI file.
+        out_filename (str): The output filename of the transformed GIfTI file.
+        affine_tfm (str): The filename of the affine component of the transform.
+        inv_warp_tfm (str): The filename of the non-linear warp component of the transform.
+
+    Returns:
+        None
+
+    Example:
+        ```python
+        transform_gifti_between_spaces(
+            "./input/brain_in_space_1.rh.white.surf.gii",
+            "./transforms/tfm_0GenericAffine.mat",
+            "./transforms/tfm_1InverseWarp.nii.gz",
+            "./output/brain_in_space_2.rh.white.surf.gii",
+        )
+        ```
     """
-    # Data in world coordinate system 1:
+    # Load the GIfTI file in world coordinate system 1:
     surf = nib.load(in_filename)
 
+    # Read the transformation components:
     tfm_aff = sitk.ReadTransform(affine_tfm)
-    tfm_inv_warp = sitk.DisplacementFieldTransform(
-        sitk.ReadImage(inv_warp_tfm))
+    tfm_inv_warp = sitk.DisplacementFieldTransform(sitk.ReadImage(inv_warp_tfm))
 
-    # Pass the points through the inverse transformation path:
+    # Create the composite transformation:
     t_new = sitk.CompositeTransform([tfm_inv_warp, tfm_aff.GetInverse()])
 
+    # Transform each vertex in the GIfTI file:
     for i in range(len(surf.darrays[0].data)):
-        v_1 = surf.darrays[0].data[i][0]
-        v_2 = surf.darrays[0].data[i][1]
-        v_3 = surf.darrays[0].data[i][2]
+        v_1, v_2, v_3 = surf.darrays[0].data[i]
 
         # Be careful to account for differences in the anatomical spaces of each dataset
         vnew = (v_1, v_2, v_3)
 
+        # Apply the transformation to the vertex:
         vtransform = t_new.TransformPoint(vnew)
 
-        surf.darrays[0].data[i][0] = vtransform[0]
-        surf.darrays[0].data[i][1] = vtransform[1]
-        surf.darrays[0].data[i][2] = vtransform[2]
+        # Update the transformed vertex in the GIfTI file:
+        surf.darrays[0].data[i] = vtransform
 
-    # Data in world coordinate system 2:
+    # Save the transformed GIfTI file in world coordinate system 2:
     nib.save(surf, out_filename)
 
 
